83c39e54a1d27e114d64f83584aba6f71c58aa89
Yiannis Spanos <yiannisvspanos@gmail.com>
2026-03-05 21:24:13 +0200

    feat: hide footer

--- app.py
+++ app.py
@@ -191,3 +191,12 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
+# Hide the "Made with Streamlit" footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
